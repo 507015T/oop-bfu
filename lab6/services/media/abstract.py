61a1033bbddd254e70dc4b081362315ffc9c4034
507015T <cuztrippie666@gmail.com>
2025-06-05 12:37:44 +0200

    feat: add sixth lab bout virtual keyboard

---
+++ lab6/services/media/abstract.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod, ABC
+
+
+class AbstractMediaPlayer(ABC):
+    @property
+    @abstractmethod
+    def is_playing(self) -> bool:
+        """Проверка запущен ли медиа плеер"""
+        ...
+
+    @abstractmethod
+    def play(self):
+        """Запуск плеера"""
+        ...
+
+    @abstractmethod
+    def stop(self):
+        """Остановка плеера"""
+        ...
